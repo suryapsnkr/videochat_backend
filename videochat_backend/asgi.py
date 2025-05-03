631968ef5cec65bad31ca3a709b5028bcab45d58
Surya Prakash Sonkar <suryapsnkr@gmail.com>
2025-05-03 20:09:53 +0530

    initial commit

--- videochat_backend/asgi.py
+++ videochat_backend/asgi.py
@@ -10,22 +10,25 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
+from django.core.asgi import get_asgi_application
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'videochat_backend.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "videochat_backend.settings")
 django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(
+                chat.routing.websocket_urlpatterns
+            )
         )
     ),
 })
+
 
 
 
