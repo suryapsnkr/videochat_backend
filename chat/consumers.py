3f4d17064b80c4d76daab24213d45c12235984d6
Surya Prakash Sonkar <suryapsnkr@gmail.com>
2025-05-04 16:09:06 +0530

    initial commit

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,33 +1,32 @@
-# consumers.py
+# matchmaker/consumers.py
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.db import database_sync_to_async
+from uuid import uuid4
 
-# In-memory waiting queue (use Redis in production)
-waiting_users = set()
+waiting_user = None  # Global variable for demo (use Redis for production)
 
-class MatchConsumer(AsyncWebsocketConsumer):
+class MatchmakerConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.user_id = self.channel_name
         await self.accept()
-        await self.find_match()
 
-    async def disconnect(self, close_code):
-        waiting_users.discard(self.user_id)
-
-    async def find_match(self):
-        if waiting_users:
-            partner_id = waiting_users.pop()
-            room_name = f"room_{self.user_id}_{partner_id}"
-
-            # Notify both users
-            await self.channel_layer.send(partner_id, {
+        global waiting_user
+        if waiting_user is None:
+            # First user, wait for match
+            waiting_user = self
+        else:
+            # Match found
+            room_name = str(uuid4())
+            await waiting_user.send(text_data=json.dumps({
                 'type': 'start.chat',
                 'room_name': room_name,
-            })
-            await self.send(json.dumps({'type': 'start.chat', 'room_name': room_name}))
-        else:
-            waiting_users.add(self.user_id)
+            }))
+            await self.send(text_data=json.dumps({
+                'type': 'start.chat',
+                'room_name': room_name,
+            }))
+            waiting_user = None  # reset
 
-    async def start_chat(self, event):
-        await self.send(json.dumps({'type': 'start.chat', 'room_name': event['room_name']}))
+    async def disconnect(self, close_code):
+        global waiting_user
+        if waiting_user == self:
+            waiting_user = None
--- chat/routing.py
+++ chat/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/match/$', consumers.MatchConsumer.as_asgi()),
-    re_path(r'ws/call/(?P<room_name>[^/]+)/$', consumers.MatchConsumer.as_asgi()),
+    re_path(r'ws/match/$', consumers.MatchmakerConsumer.as_asgi()),
+    re_path(r'ws/call/(?P<room_name>[^/]+)/$', consumers.MatchmakerConsumer.as_asgi()),
 ]
