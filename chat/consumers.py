00881f83599feaa01bc30b937d93444d6e74102d
Surya Prakash Sonkar <suryapsnkr@gmail.com>
2025-05-04 17:04:17 +0530

    initial commit

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,9 +1,8 @@
-# matchmaker/consumers.py
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from uuid import uuid4
 
-waiting_user = None  # Global variable for demo (use Redis for production)
+waiting_user = None  # Global variable for matchmaking, use Redis in production
 
 class MatchmakerConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -11,10 +10,10 @@ class MatchmakerConsumer(AsyncWebsocketConsumer):
 
         global waiting_user
         if waiting_user is None:
-            # First user, wait for match
+            # First user, wait for a match
             waiting_user = self
         else:
-            # Match found
+            # Match found, create a unique room
             room_name = str(uuid4())
             await waiting_user.send(text_data=json.dumps({
                 'type': 'start.chat',
@@ -24,9 +23,26 @@ class MatchmakerConsumer(AsyncWebsocketConsumer):
                 'type': 'start.chat',
                 'room_name': room_name,
             }))
-            waiting_user = None  # reset
+            waiting_user = None  # Reset for next match
 
     async def disconnect(self, close_code):
         global waiting_user
         if waiting_user == self:
             waiting_user = None
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        room_name = data.get('room_name')
+        if room_name:
+            # Send the message to the other user in the room
+            await self.channel_layer.send(
+                room_name,
+                {
+                    'type': 'chat_message',
+                    'message': data
+                }
+            )
+
+    async def chat_message(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps(message))
--- chat/routing.py
+++ chat/routing.py
@@ -1,8 +1,7 @@
-# routing.py
 from django.urls import re_path
-from . import consumers
+from .consumers import MatchmakerConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/match/$', consumers.MatchmakerConsumer.as_asgi()),
-    re_path(r'ws/call/(?P<room_name>[^/]+)/$', consumers.MatchmakerConsumer.as_asgi()),
+    re_path(r'ws/match/$', MatchmakerConsumer.as_asgi()),  # Matchmaker endpoint
+    re_path(r'ws/call/(?P<room_name>[^/]+)/$', MatchmakerConsumer.as_asgi()),  # Call signaling endpoint
 ]
