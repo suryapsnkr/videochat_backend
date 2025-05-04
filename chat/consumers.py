9dc613983d650bb0fa3dd9d5b7a3611c2d1cb34b
Surya Prakash Sonkar <suryapsnkr@gmail.com>
2025-05-04 19:42:22 +0530

    initial commit

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,33 +1,38 @@
+# matchmaker/consumers.py
 import json
-from uuid import uuid4
 from channels.generic.websocket import AsyncWebsocketConsumer
+from uuid import uuid4
 
-waiting_user = None  # Replace with Redis for production
+waiting_user = None  # Global variable for demo (not for production)
 
 class MatchmakerConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
+
         global waiting_user
-
         if waiting_user is None:
+            # First user connects, wait
             waiting_user = self
-            self.role = 'offerer'  # First user creates the offer
+            self.is_offerer = True
         else:
+            # Match found, generate room
             room_name = str(uuid4())
-            self.role = 'answerer'
 
-            # Notify both users
+            # Notify the first user (offerer)
             await waiting_user.send(text_data=json.dumps({
                 'type': 'start.chat',
                 'room_name': room_name,
-                'role': 'offerer',
+                'is_offerer': True
             }))
+
+            # Notify the second user (answerer)
             await self.send(text_data=json.dumps({
                 'type': 'start.chat',
                 'room_name': room_name,
-                'role': 'answerer',
+                'is_offerer': False
             }))
-            waiting_user = None  # Reset
+
+            waiting_user = None  # reset
 
     async def disconnect(self, close_code):
         global waiting_user
@@ -35,25 +40,36 @@ class MatchmakerConsumer(AsyncWebsocketConsumer):
             waiting_user = None
 
 
-class CallConsumer(AsyncWebsocketConsumer):
-    rooms = {}  # room_name: [user1, user2]
 
+class CallConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = f'call_{self.room_name}'
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
         await self.accept()
 
-        if self.room_name not in self.rooms:
-            self.rooms[self.room_name] = [self]
-        else:
-            self.rooms[self.room_name].append(self)
-
     async def disconnect(self, close_code):
-        if self.room_name in self.rooms:
-            self.rooms[self.room_name].remove(self)
-            if not self.rooms[self.room_name]:
-                del self.rooms[self.room_name]
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
     async def receive(self, text_data):
-        for peer in self.rooms.get(self.room_name, []):
-            if peer != self:
-                await peer.send(text_data)
+        data = json.loads(text_data)
+
+        # Forward signaling messages to the other peer in the room
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'signal_message',
+                'message': data
+            }
+        )
+
+    async def signal_message(self, event):
+        await self.send(text_data=json.dumps(event['message']))
